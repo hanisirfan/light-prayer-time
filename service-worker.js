012bc57b9a34f83db7afdeda3a76bad5c8df3a3f
Hanis Irfan <hanisirfan.work@gmail.com>
2025-06-27 20:54:52 +0800

    Move files locally to help with PWA cache to work offline.
    All CDNs based files has been moved to local repo to make sure PWA is working offline.

--- service-worker.js
+++ service-worker.js
@@ -10,11 +10,10 @@ const urlsToCache = [
     './manifest.json',
     './monthly-view.html', // Ensure monthly-view.html is also cached
     './audio/notification1.mp3', // Added notification sound to cache
-    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css',
-    'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css',
-    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js',
-    'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap', // Updated font to Poppins
-    'https://fonts.gstatic.com/s/poppins/v20/pxiByp8kv8JPVHnyEVuJAQtE.woff2', // Example Poppins font file (adjust if different font file is actually loaded)
+    './css/bootstrap.min.css',
+    './js/bootstrap.bundle.min.js',
+    './css/bootstrap.min.css.map',
+    './js/bootstrap.bundle.min.js.map',
     // Updated to PNG icons and new path
     './images/logo/logo-32x32.png',
     './images/logo/logo-192x192.png',
